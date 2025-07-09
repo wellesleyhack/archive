c65fa8f4b797694eeb605a2e41cc8a882a83ed8e
Marin <mg126@wellesley.edu>
2025-07-08 22:06:42 -0400

    add backgrounf
    Added background image

---
+++ src/app/archive/page.js
@@ -0,0 +1,10 @@
+import styles from './style.module.scss';
+
+export default function ArchivePage() {
+  return (
+    <main className={styles['archive-bg']}>
+      <h1>Archive</h1>
+      <p>This is the archive page.</p>
+    </main>
+  );
+}
